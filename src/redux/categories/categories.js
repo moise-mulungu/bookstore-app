73b94f8c4450e10deb3c8ddefe1df936ffa7e0e8
moise <moisemlg90@gmail.com>
2022-04-13 10:45:13 +0200

    Write categories actions and reducer

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,20 @@
+// Actions
+const CHECKSTATUS = 'bookstore-app/categories/checkStatus';
+
+// Reducer
+export default function categoriesReducer (state = [], action = {}) {
+    switch (action.type) {
+        case CHECKSTATUS:
+            return 'Under Construction';
+            default: return state
+    }
+}
+
+// Action creators
+export function CheckStatus(id) {
+    return {
+        type: CHECKSTATUS,
+        payload: { id },
+
+    }
+}
