62d48275ab07af9ad83a36c9df33b62da8c9a494
moise <moisemlg90@gmail.com>
2022-04-13 17:20:21 +0200

    Add functioanlities fro removing books

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Proptypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { del } from '../redux/books/books';
 
 export default function Book({ book }) {
+  const dispatch = useDispatch();
   return (
     <div className="bookCard">
       <div className="">
+        <p className="genre">{book.genre}</p>
         <p className="title">{book.title}</p>
         <p className="author">{book.author}</p>
-        <button type="button">Remove</button>
+        <ul>
+          <li><button type="button">Comments</button></li>
+          <li><button type="button" onClick={() => dispatch(del(book.id))}>Remove</button></li>
+          <li><button type="button">Edit</button></li>
+        </ul>
       </div>
       <div className="book-data">
         <div>
