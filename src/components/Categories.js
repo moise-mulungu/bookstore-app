a95e2a53c4ac3e3905e545c89759e8fc60c4f331
moise <moisemlg90@gmail.com>
2022-04-13 17:28:56 +0200

    Add a functionality for check status button

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 export default function Categories() {
+  const dispatch = useDispatch();
+  const currentStatus = useSelector((state) => state.CategoriesReducer);
   return (
-    <button type="button">Check status</button>
+    <div>
+      <button type="button" onClick={() => dispatch(checkStatus())}>Check status</button>
+      {currentStatus}
+    </div>
   );
 }
