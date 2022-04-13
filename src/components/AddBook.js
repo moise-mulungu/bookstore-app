a8bbcf710146fb49591254f3c135aea1ddc7a57a
moise <moisemlg90@gmail.com>
2022-04-13 17:10:02 +0200

    Add functionalities for adding books

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { add } from '../redux/books/books';
 
 export default function AddBook() {
+  const dispatch = useDispatch();
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+
+  function submitBookToStore (event) {
+    event.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+      genre: document.getElementById('bookgenre').value,
+    };
+    dispatch(add(newBook));
+    setTitle('');
+    setAuthor('');
+  }
   return (
     <div>
       <h2>Add New Book</h2>
-      <form>
-        <input type="text" placeholder="Add book..." />
-        <select key="author">
-          <option value="Category">all authors</option>
+      <form onSubmit={submitBookToStore}>
+        <input type="text" placeholder="Add book..." value={title} onChange={(event) => setTitle(event.target.value)} required/>
+        <input type="text" placeholder="Book title" value={author} onChange={(event) => setAuthor(event.target.value)} required/>
+        <select id='bookgenre' key="genre" defaultValue="Genre">
+          <option value="all authors">all authors</option>
         </select>
         <button type="submit">Add Book</button>
       </form>
