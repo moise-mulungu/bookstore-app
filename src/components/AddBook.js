5b57244f4c6734c2f986eae90feff8a279093b8d
moise <moisemlg90@gmail.com>
2022-04-14 05:41:52 +0200

    Fix the useEffect dependency

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { add } from '../redux/books/books';
 
