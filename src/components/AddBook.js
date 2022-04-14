0f92459e33d56554c0a1fa83b3e672be357c26c4
moise <moisemlg90@gmail.com>
2022-04-14 05:02:53 +0200

    Refactor the add book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -7,27 +7,35 @@ export default function AddBook() {
   const dispatch = useDispatch();
   const [author, setAuthor] = useState('');
   const [title, setTitle] = useState('');
+  const [categoryState, setCategory] = useState('');
 
   function submitBookToStore(event) {
     event.preventDefault();
     const newBook = {
-      id: uuidv4(),
+      item_id: uuidv4(),
       title,
       author,
-      genre: document.getElementById('bookgenre').value,
+      category: categoryState,
     };
     dispatch(add(newBook));
     setTitle('');
     setAuthor('');
+    setCategory('');
   }
+
   return (
     <div>
       <h2>Add New Book</h2>
       <form onSubmit={submitBookToStore}>
         <input type="text" placeholder="Add book..." value={title} onChange={(event) => setTitle(event.target.value)} required />
         <input type="text" placeholder="Book title" value={author} onChange={(event) => setAuthor(event.target.value)} required />
-        <select id="bookgenre" key="genre" defaultValue="Genre">
-          <option value="all authors">all authors</option>
+        <select type="text" onChange={(event) => setCategory(event.target.value)}>
+          <option value="category">category</option>
+          <option value="Classics">Classics</option>
+          <option value="Romance">Romance</option>
+          <option value="Adventure">Adventure</option>
+          <option value="Science">Science</option>
+          <option value="Historical">Historical</option>
         </select>
         <button type="submit">Add Book</button>
       </form>
